5830d79985d6ec5068a6705b56a5b8a05b5597dd
ayobamy <ayobamyahmed@gmail.com>
2022-09-03 19:40:44 +0000

    [update]: made changes to file

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -3,29 +3,27 @@ import random
 
 # 'r' for rock, 'p' for paper, and 's' for scissors
     # r > s, s > p, p > r
-    
+
 bot_choice = ['r', 'p', 's']
 
 def play():
     player = input("R for rock, P for paper, S for Scissors.\nWhat's your choice?  ")
     player = player.lower()
     computer = random.choice(bot_choice)
-    
 
     if (player == computer):
         return "It\'s a tie!"
-        
+
     if is_win(player, computer):
         return "You Won!"
-    
+
     return "Sorry! You lost!"
-    
+
 def is_win(player, computer):
     if ((player == 'r' and computer == 's') or (player == 's' and computer == 'p') or (player == 'p' and computer == 'r')):
-        
+
         return True
-        # print(f"You choose {player}, computer choose {computer}.\n Yaaaaay! You won!")
-        
+
 print(play())
 
 
